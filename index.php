1ffc28ac16cfd3353f355c535600ecf5a4e246d1
rjrochak <rochak.jaitly@gmail.com>
2023-08-17 15:59:07 +0530

    boilerplate

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php
+include("includes/header.php");
+    ?>
+    
+ 
+    <?php
+include("includes/footer.php");
+    ?>
+<script src="js/bootsrap.js"></script>
+<script src="js/custom.js"></script>
+<script src="js/popper.js"></script>
+</body>
+</html>
