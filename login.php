5fa0c969ad55465f1aec89b403b02d0cb5b0b80d
Aktechweb09 <techweb029@gmail.com>
2023-08-20 09:00:02 +0530

    change

--- login.php
+++ login.php
@@ -8,14 +8,58 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <?php
-include("includes/header.php");
-    ?>
-    
+
+
+
+<?php
+include 'inc/header.php';
+Session::CheckLogin();
+?>
+
+
+
+<?php
+ $logMsg = Session::get('logMsg');
+                if (isset($logMsg)) {
+                  echo $logMsg;
+                }
+                $msg = Session::get('msg');
+                if (isset($msg)) {
+                  echo $msg;
+                }
+                Session::set("msg", NULL);
+                Session::set("logMsg", NULL);
+
+ ?>
+
+<div class="card ">
+        <div class="card-header">
+          <h3 class='text-center'><i class="fas fa-sign-in-alt mr-2"></i>User login</h3>
+        </div>
+        <div class="card-body">
+            <div style="width:450px; margin:0px auto">
+            <form class="" action="" method="post">
+                <div class="form-group">
+                  <label for="email">Email address</label>
+                  <input type="email" name="email"  class="form-control">
+                </div>
+                <div class="form-group">
+                  <label for="password">Password</label>
+                  <input type="password" name="password"  class="form-control">
+                </div>
+                <div class="form-group">
+                  <button type="submit" name="login" class="btn btn-success">Login</button>
+                </div>
+            </form>
+          </div>
+        </div>
+      </div>
+
+  <?php
+  include 'include/footer.php';
+
+  ?>
  
-    <?php
-include("includes/footer.php");
-    ?>
 <script src="js/bootsrap.js"></script>
 <script src="js/custom.js"></script>
 <script src="js/popper.js"></script>
--- registration.php
+++ registration.php
@@ -22,7 +22,7 @@
         }
     </style>
   </head>
-  <body style="backgroud-image:url('bg img.jpg') !important;" >
+  <body style="backgroud-image:url('public/bgimg.jpg') !important;">
     <form action="#" method="POST">
   <section class="h-100 bg-dark">
   <div class="container py-5 h-100">
@@ -31,10 +31,10 @@
         <div class="card card-registration my-4">
           <div class="row g-0">
             <div class="col-xl-6 d-none d-xl-block">
-              <img src="pex7.jpg"
+              <img src="public/pex7.jpg"
                 alt="Sample photo" class="img-fluid"
                 style="border-top-left-radius: .25rem; border-bottom-left-radius: .25rem; height:100% !important; " />
-            </div>
+      </div>
             <div class="col-xl-6">
               <div class="card-body p-md-5 text-black">
                 <h3 class="mb-5 text-uppercase">Student registration form</h3>
@@ -42,13 +42,13 @@
                 <div class="row">
                   <div class="col-md-6 mb-4">
                     <div class="form-outline">
-                      <input type="text" id="form3Example1m" class="form-control form-control-lg" name="firstname" />
+                      <input type="text" id="form3Example1m" class="form-control form-control-lg" name="firsname"required />
                       <label class="form-label" for="form3Example1m">First name</label>
                     </div>
                   </div>
                   <div class="col-md-6 mb-4">
                     <div class="form-outline">
-                      <input type="text" id="form3Example1n" class="form-control form-control-lg" name="lastname" />
+                      <input type="text" id="form3Example1n" class="form-control form-control-lg" name="lastname" required />
                       <label class="form-label" for="form3Example1n">Last name</label>
                     </div>
                   </div>
@@ -57,20 +57,20 @@
                 <div class="row">
                   <div class="col-md-6 mb-4">
                     <div class="form-outline">
-                      <input type="text" id="form3Example1m1" class="form-control form-control-lg" name="mname" />
+                      <input type="text" id="form3Example1m1" class="form-control form-control-lg" name="mname" required />
                       <label class="form-label" for="form3Example1m1">Mother's name</label>
                     </div>
                   </div>
                   <div class="col-md-6 mb-4">
                     <div class="form-outline">
-                      <input type="text" id="form3Example1n1" class="form-control form-control-lg" name="fname" />
+                      <input type="text" id="form3Example1n1" class="form-control form-control-lg" name="fname" required />
                       <label class="form-label" for="form3Example1n1">Father's name</label>
                     </div>
                   </div>
                 </div>
 
                 <div class="form-outline mb-4">
-                  <input type="text" id="form3Example8" class="form-control form-control-lg" name="address" />
+                  <input type="text" id="form3Example8" class="form-control form-control-lg" name="address" required />
                   <label class="form-label" for="form3Example8">Address</label>
                 </div>
 
@@ -98,9 +98,9 @@
                 <div class="row">
                   <div class="col-md-6 mb-4">
 
-                    <select class="select" name="state">
+                    <select class="select" name="state" required>
                       <option value="select state" >State</option>
-                      <option value="chh"  >option1</option>
+                      <option value="chh"  >chh</option>
                       <option value="3">Option 2</option>
                       <option value="4"  >Option 3</option>
                     </select>
@@ -108,7 +108,7 @@
                   </div>
                   <div class="col-md-6 mb-4">
 
-                    <select class="select" name="city">
+                    <select class="select" name="city" required>
                       <option value="select city"  >City</option>
                       <option value="raipur"   >Raipur</option>
                       <option value="3"   >Option 2</option>
@@ -119,32 +119,39 @@
                 </div>
 
                 <div class="form-outline mb-4">
-                  <input type="date" id="form3Example9" class="form-control form-control-lg"  name="dateofbirth" />
+                  <input type="date" id="form3Example9" class="form-control form-control-lg"  name="dateofbirth" required />
                   <label class="form-label" for="form3Example9">DOB</label>
                 </div>
 
                 <div class="form-outline mb-4">
-                  <input type="text" id="form3Example90" class="form-control form-control-lg" name="pincode" />
+                  <input type="number" id="form3Example90" class="form-control form-control-lg" name="pncode" required />
                   <label class="form-label" for="form3Example90">Pincode</label>
                 </div>
-
+<!-- 
                 <div class="form-outline mb-4">
-                  <select type="text" id="form3Example99" class="form-control form-control-lg" name="course" >
-                  <option value="IT"  >IT</option>
+                  <input type="text" id="form3Example99" class="form-control form-control-lg" name="course" />
+                  <label class="form-label" for="form3Example99">Course</label>
+                </div> -->
+                  <div class="form-outline mb-4">
+                  <select type="text" id="form3Example99" class="form-control form-control-lg" name="course" required >
+                      <option value="null"  >seclect</option>
+                      <option value="IT"  >IT</option>
                       <option value="Arts"   >Arts</option>
                       <option value="Commerce"   >Commerce </option>
                  </select>
                   <label class="form-label" for="form3Example99">Course</label>
                 </div>
 
+
+
                 <div class="form-outline mb-4">
-                  <input type="text" id="form3Example97" class="form-control form-control-lg"  name="email" />
+                  <input type="email" id="form3Example97" class="form-control form-control-lg"  name="email" required />
                   <label class="form-label" for="form3Example97">Email ID</label>
                 </div>
 
                 <div class="d-flex justify-content-end pt-3">
                   <button type="button" class="btn btn-light btn-lg">Reset all</button> 
-                  <input type="submit" name="submitform " value="Sign Up" class="btn btn-warning btn-lg ms-2">
+                  <input type="submit" name="submit" value="register" class="btn btn-warning btn-lg ms-2">
                 </div>
 
               </div>
@@ -170,10 +177,10 @@
        extract($_POST);
 
         //database connection 
-       $conn = mysqli_connect("localhost","root","","AssignmentCollector");
+       $conn = mysqli_connect("localhost","root","","assignmentcollector");
 
         //inser query (insert into tablename )
-       $data = "INSERT into registration(firstname,lastname,mname,fname,address,gender,state,city,dateofbirth,pincode,course,email) VALUES('$firstname','$lastname','$mname', '$fname','$address','$gender','$state','$city','$dateofbirth','$pincode','$course','$email')";
+       $data = "INSERT into users(first_name,last_name,mother_name,father_name,address,gender,state,city,dob,pincode,course,email_id) VALUES('$firsname','$lastname','$mname', '$fname','$address','$gender','$state','$city','$dateofbirth','$pncode','$course','$email')";
 
         //run both query 
        $query = mysqli_query($conn, $data);
@@ -183,12 +190,32 @@
        if ($query) {
 
           // Display SweetAlert success message
-              echo "good";
+            //   echo '<script>
+            //     Swal.fire({
+            //         title: "Success!",
+            //         text: "Data is added successfully",
+            //         icon: "success",
+            //         confirmButtonText: "OK"
+            //     });
+            // </script>';
+        echo "success ";
 
        }
        else
        {
          echo "ooppps something went wrong ! ";
+       }
+       if ($query) {
+         // code...
+
+            echo '<script>
+                Swal.fire({
+                    title: "Success!",
+                    text: "Data is added successfully",
+                    icon: "success",
+                    confirmButtonText: "OK"
+                });
+            </script>';
        }
 
     }
