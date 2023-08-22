993e34cf048ec0637a32606fbd699a6d8f2131ef
Aktechweb09 <techweb029@gmail.com>
2023-08-22 14:29:33 +0530

    yes

--- datacon.php
+++
@@ -1,9 +0,0 @@
-<?php
-$hostname = "localhost";
-$username = "root";
-$password = "";
-$database = "assignmentcollector";   // change this name 
-
-
-$connect = mysqli_connect($hostname,$username,$password,$database);
-?>
